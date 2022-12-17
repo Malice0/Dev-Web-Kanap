f7d1592e0370857f03b4cb3e66517b211a3e1b47
Malice0 <Amalicia29@gmail.com>
2022-12-16 23:06:55 -0300

    etape 6

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,58 @@
+let productId = new URLSearchParams(window.location.search).get("id");
+// console.log(productId);
+
 let productImg = document.createElement("img");
 productImg.setAttribute("src", "../images/logo.png");
 productImg.setAttribute("alt", "Photographie d'un canapé");
 let itemImg = document.querySelector(".item__img");
 itemImg.appendChild(productImg);
 
+fetch("http://localhost:3000/api/products/" + productId)
+  .then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+  })
+  .then((product) => {
+    let productName = document.getElementById("title");
+    productName.textContent = product.name;
+
+    let productPrice = document.getElementById("price");
+    productPrice.textContent = product.price;
+
+    let productDescription = document.getElementById("description");
+    productDescription.textContent = product.description;
+
+    // console.log(product);
+
+    let productColor = document.querySelector("#colors");
+    // productColor.
+
+    // console.log(productColor);
+
+
+    product.colors.forEach((c) => {
+
+      let productOption = document.createElement("option");
+      productColor.appendChild(productOption);
+
+      productOption.setAttribute("value", c);
+      productOption.textContent = c;
+
+      console.log(productOption);
+      // console.log(c);
+    });
+  });
+
+
+// _id
+// .querySelector("#quantity")
+// .querySelector("#colors")
+// !use localStorage!
+// ++ dans le panier, il faut que la value = 0 += product
+// += dans le panier si _id + #colors on joute juste la quantité
+// Je pense direct à switch car if else il ya aura trop de condition
+
+//  !Attention multiplication inutile des products! 
+//   
+//   
--- front/js/script.js
+++ front/js/script.js
@@ -1,43 +1,45 @@
 fetch("http://localhost:3000/api/products")
-    .then (function(response) {
-        if (response.ok) {
-            return response.json();
-        }
-    })
-    .then ((products) => {
-        products.forEach((p) => { console.log(p)
-            let productLink = document.createElement("a");
-            productLink.setAttribute("href", `product.html?id=${(p)._id}`);
-            let item = document.getElementById("items");
-            items.appendChild(productLink);
-            
-            let productArticle = document.createElement("article");
-            productLink.appendChild(productArticle);
+  .then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+  })
+  .then((products) => {
+    products.forEach((p) => {
+      console.log(p);
+      let productLink = document.createElement("a");
+      productLink.setAttribute("href", `product.html?id=${p._id}`);
+      let items = document.getElementById("items");
+      items.appendChild(productLink);
 
-            let productImg = document.createElement("img");
-            productImg.setAttribute("src", (p).imageUrl);
-            productImg.setAttribute("alt", (p).altTxt);
-            productArticle.appendChild(productImg);
-            
-            let productName = document.createElement("h3");
-            productName.classList.add("productName");
-            productName.textContent = (p).name;
-            productArticle.appendChild(productName);
+      let productArticle = document.createElement("article");
+      productLink.appendChild(productArticle);
 
-            let productDescription = document.createElement("p");
-            productArticle.appendChild(productDescription);
-            productDescription.classList.add("productDescription");
-            productDescription.textContent = (p).description;
-        });
-    })
-    .catch((error) => {
-        console.log("Le chargement des produits a rencontré un problème" + error);
-        let errorMessage = document.createElement("h2");
-        errorMessage.textContent = "Nous avons rencontrez des difficultés techniques et ne pouvons pas présenter les articles pour le moment. Nos équipes à l'oeuvre pour résoudre ce problème dans les plus brefs délais. Nous vous invitons à réessayer ultérieurement et nous nous excusons pour la gène occasionnée."
-        errorMessage.style.padding = "15px";
-        console.log (productsList)
-        productsList.appendChild(errorMessage);
-    })
+      let productImg = document.createElement("img");
+      productImg.setAttribute("src", p.imageUrl);
+      productImg.setAttribute("alt", p.altTxt);
+      productArticle.appendChild(productImg);
+
+      let productName = document.createElement("h3");
+      productName.classList.add("productName");
+      productName.textContent = p.name;
+      productArticle.appendChild(productName);
+
+      let productDescription = document.createElement("p");
+      productArticle.appendChild(productDescription);
+      productDescription.classList.add("productDescription");
+      productDescription.textContent = p.description;
+    });
+  })
+  .catch((error) => {
+    console.log("Le chargement des produits a rencontré un problème" + error);
+    let errorMessage = document.createElement("h2");
+    errorMessage.textContent =
+      "Nous avons rencontrez des difficultés techniques et ne pouvons pas présenter les articles pour le moment. Nos équipes à l'oeuvre pour résoudre ce problème dans les plus brefs délais. Nous vous invitons à réessayer ultérieurement et nous nous excusons pour la gène occasionnée.";
+    errorMessage.style.padding = "15px";
+    console.log(productsList);
+    productsList.appendChild(errorMessage);
+  });
 
 // const productsList = document.getElementById("items")
 // console.log (productsList)
